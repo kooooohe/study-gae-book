24e9b415fd08574d2d060206791d9b65481b25da
kooooohe <kooooohe@1211lta@gmail.com>
2020-01-29 14:24:50 +0900

    test

--- helloworld/main.go
+++ helloworld/main.go
@@ -16,7 +16,7 @@ func main() {
 		log.Printf("Defaulting to port %s", port)
 	}
 
-	log.Printf("Listening on port %s", port)
+	log.Printf("Listening  on port %s", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
 
